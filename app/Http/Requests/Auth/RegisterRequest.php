6f9529588230c4934648358edd710e67d832eb3d
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-11-12 17:18:39 +0800

    feat: Add disposable email validation to registration

--- app/Http/Requests/Auth/RegisterRequest.php
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -3,15 +3,21 @@
 namespace App\Http\Requests\Auth;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\App;
 use Illuminate\Validation\Rules\Password;
 
 class RegisterRequest extends FormRequest
 {
     public function rules(): array
     {
+        return App::isProduction() ? $this->productionRules() : $this->localRules();
+    }
+
+    public function productionRules(): array
+    {
         return [
             'name'     => ['required', 'string', 'max:255', 'min:3'],
-            'email'    => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'email'    => ['required', 'string', 'disposable_email', 'email:rfc,dns,spoof,strict', 'max:255', 'unique:users'],
             'password' => ['required', 'confirmed',
                 Password::min(8)
                     ->letters()
@@ -19,6 +25,18 @@ class RegisterRequest extends FormRequest
                     ->mixedCase() // lower-case and upper-case letter
                     ->symbols()
                     ->uncompromised(),
+            ],
+            'password_confirmation' => ['required'],
+        ];
+    }
+
+    public function localRules(): array
+    {
+        return [
+            'name'     => ['required', 'string', 'max:255', 'min:3'],
+            'email'    => ['required', 'string',  'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'confirmed',
+                Password::min(8),
             ],
             'password_confirmation' => ['required'],
         ];
