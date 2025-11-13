98b9f79d2c33aca1eb3c5c943570549c1ddc7882
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-11-13 10:07:44 +0800

    refactor: Separate trait uses

--- app/Mail/Users/WelcomeMail.php
+++ app/Mail/Users/WelcomeMail.php
@@ -12,7 +12,8 @@ use Illuminate\Queue\SerializesModels;
 
 class WelcomeMail extends Mailable implements ShouldQueue
 {
-    use Queueable, SerializesModels;
+    use Queueable;
+    use SerializesModels;
 
     public function __construct(public readonly User $user) {}
 
