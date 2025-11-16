7a5b80b618503d24a6510b605e519b22842c3be3
Josie Darang <18107626+MarJose123@users.noreply.github.com>
2025-11-16 14:42:50 +0800

    Cookie consent banner (#4)
    feat: Add cookie consent banner

--- resources/js/layouts/default.vue
+++ resources/js/layouts/default.vue
@@ -1,19 +1,47 @@
 <script setup lang="ts">
 import { useColorUi } from '@/composables/useColorUi'
+import { useStorage } from '@vueuse/core'
 
 const { primaryColor } = useColorUi()
+const toast = useToast()
 const toaster = { expand: false, progress: false, color: primaryColor.value, max: 3 }
+
+const cookie = useStorage('cookie-consent', 'pending')
+if (cookie.value !== 'accepted') {
+    toast.add({
+        title: 'We use first-party cookies to enhance your experience on our website.',
+        duration: 0,
+        close: false,
+        actions: [
+            {
+                label: 'Accept',
+                color: 'neutral',
+                variant: 'outline',
+                onClick: () => {
+                    cookie.value = 'accepted'
+                },
+            },
+            {
+                label: 'Remind me later',
+                color: 'neutral',
+                variant: 'ghost',
+            },
+        ],
+    })
+}
 </script>
 
 <template>
-    <UApp :toaster="toaster">
-        <UMain>
-            <UDashboardGroup as="div" storage="local" storage-key="dashboard" :persistent="true">
-                <SidebarNavigation />
-                <slot />
-            </UDashboardGroup>
-        </UMain>
-    </UApp>
+    <Suspense>
+        <UApp :toaster="toaster">
+            <UMain>
+                <UDashboardGroup as="div" storage="local" storage-key="dashboard" :persistent="true">
+                    <SidebarNavigation />
+                    <slot />
+                </UDashboardGroup>
+            </UMain>
+        </UApp>
+    </Suspense>
 </template>
 
 <style scoped></style>
