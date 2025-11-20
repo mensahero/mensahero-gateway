3e1e8bdd8c910300da83628a3129f3715779e690
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-11-20 15:39:21 +0800

    refactor: Relocate cookie consent to Welcome page

--- resources/js/layouts/default.vue
+++ resources/js/layouts/default.vue
@@ -1,32 +1,5 @@
 <script setup lang="ts">
-import { useStorage } from '@vueuse/core'
-
-const toast = useToast()
 const toaster = { expand: false, progress: false, max: 4 }
-
-const cookie = useStorage('cookie-consent', 'pending')
-if (cookie.value !== 'accepted') {
-    toast.add({
-        title: 'We use first-party cookies to enhance your experience on our website.',
-        duration: 0,
-        close: false,
-        actions: [
-            {
-                label: 'Accept',
-                color: 'neutral',
-                variant: 'outline',
-                onClick: () => {
-                    cookie.value = 'accepted'
-                },
-            },
-            {
-                label: 'Remind me later',
-                color: 'neutral',
-                variant: 'ghost',
-            },
-        ],
-    })
-}
 </script>
 
 <template>
